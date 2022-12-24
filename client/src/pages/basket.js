444af33a92bc857f4f2b7f65614212dff5289086
DGAFBB <71436333+DGAFBB@users.noreply.github.com>
2022-12-24 07:13:03 +0300

    в ролях

--- client/src/pages/basket.js
+++ client/src/pages/basket.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import {Container} from "react-bootstrap";
 
 const Basket = () => {
     return (
-        <div>
-            
-        </div>
+        <Container><Container/>
     );
 };
 
