cc45ebb1a7c2bf67df23e14bede99cf1da30aaea
DGAFBB <71436333+DGAFBB@users.noreply.github.com>
2022-12-24 07:37:02 +0300

    немного

--- client/src/pages/basket.js
+++ client/src/pages/basket.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
+import "../styles/fonts/fonts.css"
+import "../styles/Buttons/button.css"
+import {Link} from "react-router-dom";
+import {DELIVERY_ROUTE} from "../utils/consts";
 
 const Basket = () => {
     return (
-        <Container><Container/>
+        <Container>
+            <Row>
+                <text style={{marginTop:"4%", marginBottom:"7%", marginRight:"auto", marginLeft:0}} className="heading2">Оформление заказа</text>
+            </Row>
+            <Row>
+
+            </Row>
+            <Row>
+                <Link to={DELIVERY_ROUTE} style={{marginTop:"5%", marginBottom:"7%", marginRight:0, marginLeft:"auto"}}>
+                <button style={{width:"160%", height:"140%", marginRight:0, marginLeft:"auto"}} className="brown-button">Оформить заказ</button>
+                </Link>
+            </Row>
+        </Container>
     );
 };
 
