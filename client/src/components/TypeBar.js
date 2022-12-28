e1d8d3aacc325b008b9b2ec4bcd40d9ce3c5191b
DGAFBB <71436333+DGAFBB@users.noreply.github.com>
2022-12-28 21:58:00 +0300

    похуй

--- client/src/components/TypeBar.js
+++ client/src/components/TypeBar.js
@@ -7,19 +7,31 @@ import {Col, Row} from "react-bootstrap";
 const TypeBar = observer(() => {
     const {product} = useContext(Context)
     return (
-        <Row style={{backgroundColor:"#5D4037"}} className="justify-content-around">
-            <Col>
-            {product.types.map(type =>
-                <Col.Item
-                    style={{cursor: 'pointer'}}
-                    active={type.id === product.selectedType.id}
-                    onClick={() => product.setSelectedType(type)}
-                    key={type.id}
-                >
-                   <text className="heading2">{type.name}</text>
-                </Col.Item>
-            )}
-            </Col>
+        <Row style={{backgroundColor:"#5D4037", paddingTop:"2%", paddingBottom:"2%"}} className="justify-content-md-center">
+            <Row style={{width:"50%", left:"25%"}} className="justify-content-center row-border">
+                <Col style={{left:"5%"}}>
+                    <text className="heading2" >ghnbbcvdfyh</text>
+                </Col>
+                <Col >
+                    <text className="heading2">ghnbbcvdfyh</text>
+                </Col>
+                <Col >
+                    <text className="heading2">ghnbbcvdfyh</text>
+                </Col>
+                <Col >
+                    <text className="heading2">ghnbbcvdfyh</text>
+                </Col>
+                {/*{product.types.map(type =>*/}
+                {/*    <Col.Item*/}
+                {/*        style={{cursor: 'pointer'}}*/}
+                {/*        active={type.id === product.selectedType.id}*/}
+                {/*        onClick={() => product.setSelectedType(type)}*/}
+                {/*        key={type.id}*/}
+                {/*    >*/}
+                {/*       <text className="heading2">{type.name}</text>*/}
+                {/*    </Col.Item>*/}
+                {/*)}*/}
+            </Row>
         </Row>
     );
 });
